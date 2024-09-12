fab3befe867ce4b2bba7d1c14f7c7a147a58e992
Angela Gnazzo <martinpedraza1979@gmail.com>
2024-09-12 17:51:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// select the toggle button
+const toggleBtn = document.querySelector(".header__toggle-btn")
+// select the header
+const header = document.querySelector(".header")
+
+toggleBtn.addEventListener("click", () => {
+	// add or remove the "header-open" class from the header element
+	// write your code here
+	console.log("run the function here !!")
+})
